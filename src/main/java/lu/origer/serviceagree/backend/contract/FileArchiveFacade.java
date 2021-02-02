2a24bd4e2659984fc8d069a026ddaf6af7628571
Kai Kremer <kremerkp@gmail.com>
2021-02-02 14:03:46 +0100

    complete seperate instance setup

--- src/main/java/lu/origer/serviceagree/backend/contract/FileArchiveFacade.java
+++ src/main/java/lu/origer/serviceagree/backend/contract/FileArchiveFacade.java
@@ -34,8 +34,7 @@ import lu.origer.serviceagree.models.main.Users;
 import lu.origer.serviceagree.models.synch.ServiceHistory;
 
 @Stateless
-public class FileArchiveFacade extends AbstractFacade<FileArchive>
-{
+public class FileArchiveFacade extends AbstractFacade<FileArchive> {
 
 	public static final String PATH_TO_FILEARCHIVE_OFFER = "\\\\SERVER-SQL\\upload\\offer";
 	public static final String PATH_TO_FILEARCHIVE_INVOICE = "\\\\SERVER-SQL\\upload\\invoice";
@@ -52,19 +51,16 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 	@PersistenceContext(unitName = "ORIGERPU")
 	private EntityManager em;
 
-	public FileArchiveFacade()
-	{
+	public FileArchiveFacade() {
 		super(FileArchive.class);
 	}
 
 	@Override
-	protected EntityManager getEntityManager()
-	{
+	protected EntityManager getEntityManager() {
 		return em;
 	}
 
-	public FileArchive findForHistory(ServiceHistory sh)
-	{
+	public FileArchive findForHistory(ServiceHistory sh) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -72,8 +68,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.singleResult(f);
 	}
 
-	public List<FileArchive> findFilesByUrl(String url)
-	{
+	public List<FileArchive> findFilesByUrl(String url) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -81,8 +76,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findFilesByName(String name)
-	{
+	public List<FileArchive> findFilesByName(String name) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -90,8 +84,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findAllBuildingSiteFilesForBS(Integer bsId)
-	{
+	public List<FileArchive> findAllBuildingSiteFilesForBS(Integer bsId) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -99,8 +92,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findAllBuildingSiteFilesForCustomer(Integer user)
-	{
+	public List<FileArchive> findAllBuildingSiteFilesForCustomer(Integer user) {
 		Users us = userfacade.find(user);
 
 		List<Integer> bsList = buildingSiteFacade.findBuildingSiteForCustomer(us);
@@ -111,8 +103,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findAllBuildingSiteFiles()
-	{
+	public List<FileArchive> findAllBuildingSiteFiles() {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -120,8 +111,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findfilesForBuildingSite(BuildingSite bs)
-	{
+	public List<FileArchive> findfilesForBuildingSite(BuildingSite bs) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -129,8 +119,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findFilesForOffer(Offers offer)
-	{
+	public List<FileArchive> findFilesForOffer(Offers offer) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -138,8 +127,7 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 	}
 
-	public List<FileArchive> findFilesForInvoice(Invoice invoice)
-	{
+	public List<FileArchive> findFilesForInvoice(Invoice invoice) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -147,40 +135,36 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.list(f);
 
 	}
-	
-	public FileArchive findApprovalSignForServiceHistoryRepair(final Long id)
-	{
+
+	public FileArchive findApprovalSignForServiceHistoryRepair(final Long id) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
 		q.where(f.serviceHistory.id.eq(id).and(f.signature.isTrue()));
 		FileArchive result = q.singleResult(f);
-		if (result != null)
-		{
+		if (result != null) {
 			result.setUrl(result.getUrl().replace("/etc", ""));
 		}
 
 		return result;
 	}
 
-	public String findApprovalSignForService(Service service)
-	{
+	public String findApprovalSignForService(Service service) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
 		q.where(f.service.eq(service));
 		q.where(f.signature.isTrue()).orderBy(f.id.desc());
 		String result = q.singleResult(f.url);
-		if (result != null)
-		{
+		if (result != null) {
 			result = result.replace("/etc", "");
+			result = result.replace("/usr/local/tomee/", "/origer/");
 		}
 
-		return q.singleResult(f.url);
+		return result;
 	}
-	
-	public FileArchive findApprovalSignForTimeRecording(final Long recordId)
-	{
+
+	public FileArchive findApprovalSignForTimeRecording(final Long recordId) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -188,31 +172,28 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		q.where(f.signature.isTrue()).orderBy(f.id.desc());
 		return q.singleResult(f);
 	}
-	
-	public FileArchive findApprovalSignForService(final Integer serviceId)
-	{
+
+	public FileArchive findApprovalSignForService(final Integer serviceId) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
-		q.from(f).where(f.service.id.eq(serviceId).and(f.signature.isTrue()));		
-		return q.singleResult(f);		
+		q.from(f).where(f.service.id.eq(serviceId).and(f.signature.isTrue()));
+		return q.singleResult(f);
 	}
-	
-	public FileArchive findApprovalSignForService(final Integer serviceId, final Date createDate)
-	{
+
+	public FileArchive findApprovalSignForService(final Integer serviceId, final Date createDate) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		final Calendar cal = new GregorianCalendar();
 		cal.setTime(createDate);
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);		
+		cal.set(Calendar.SECOND, 0);
 		cal.set(Calendar.MILLISECOND, 0);
-		q.from(f).where(f.service.id.eq(serviceId).and(f.signature.isTrue().and(f.createDate.eq(cal.getTime()))));		
-		return q.singleResult(f);		
+		q.from(f).where(f.service.id.eq(serviceId).and(f.signature.isTrue().and(f.createDate.eq(cal.getTime()))));
+		return q.singleResult(f);
 	}
-	
-	public FileArchive findApprovalSignForHistory(Long historyId)
-	{
+
+	public FileArchive findApprovalSignForHistory(Long historyId) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
@@ -220,13 +201,13 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 		return q.singleResult(f);
 	}
 
-	public String findApprovalSignNameForService(Service service)
-	{
+	public String findApprovalSignNameForService(Service service) {
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
 		q.where(f.service.eq(service));
-		q.where(f.signature.isTrue()).orderBy(f.id.desc());;		
+		q.where(f.signature.isTrue()).orderBy(f.id.desc());
+		;
 		String result = q.singleResult(f.description);
 
 		return q.singleResult(f.description);
@@ -238,13 +219,10 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 	 * @param url
 	 * @return
 	 */
-	public String getFileName(final String name)
-	{
+	public String getFileName(final String name) {
 		int count = 0;
-		for (FileArchive f : this.findFilesByName(name))
-		{
-			if (f.getName().contains(name))
-			{
+		for (FileArchive f : this.findFilesByName(name)) {
+			if (f.getName().contains(name)) {
 				count++;
 			}
 		}
@@ -257,14 +235,11 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 	 * @param url
 	 * @return
 	 */
-	public String getUrl(final String url)
-	{
+	public String getUrl(final String url) {
 		String newUrl = url;
 		int count = 0;
-		for (FileArchive f : this.findFilesByUrl(url))
-		{
-			if (f.getUrl().contains(url))
-			{
+		for (FileArchive f : this.findFilesByUrl(url)) {
+			if (f.getUrl().contains(url)) {
 				count++;
 			}
 		}
@@ -277,55 +252,43 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 	 * @param archive
 	 * @return true on success, false if an error occurred.
 	 */
-	public Boolean deleteFile(final FileArchive archive)
-	{
+	public Boolean deleteFile(final FileArchive archive) {
 		Boolean result = true;
 		final File file = new File(sessionBean.getApplicationBean().getPathToFileArchiv() + "/" + archive.getSubfolder()
 				+ "/" + archive.getName());
-		if (file.exists())
-		{
+		if (file.exists()) {
 			System.out.println("File found: " + file.getAbsolutePath());
-			if (file.delete())
-			{
+			if (file.delete()) {
 				System.out.println("File deleted for " + archive.getId() + " removing DB entry...");
-				try
-				{
+				try {
 					this.remove(archive);
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					System.out.println("Error removing file archive " + archive.getId());
 					e.printStackTrace();
 					result = false;
 				}
 			}
-		}
-		else
-		{
+		} else {
 			System.out.println("No file found for " + file.getAbsolutePath());
 			result = false;
 		}
 
 		return result;
 	}
-	
+
 	/**
 	 * Returns the first service id for the selected archive.
 	 * 
 	 * @param archiveId
 	 * @return building site id if found, null on error or no result.
 	 */
-	public Integer findServiceIdByFileArchive(final Integer archiveId)
-	{
+	public Integer findServiceIdByFileArchive(final Integer archiveId) {
 		Integer result = null;
-		if(archiveId != null)
-		{
-			final String query = "SELECT ONE ser.id "
-					+ "FROM service ser "
+		if (archiveId != null) {
+			final String query = "SELECT ONE ser.id " + "FROM service ser "
 					+ "INNER JOIN service_contract con ON con.id = ser.fk_service_contract "
 					+ "INNER JOIN building_site bs ON bs.id = con.fk_building_site "
-					+ "INNER JOIN filearchive fa ON bs.id = fa.fk_building_site"
-					+ "WHERE fa.id = " + archiveId;
+					+ "INNER JOIN filearchive fa ON bs.id = fa.fk_building_site" + "WHERE fa.id = " + archiveId;
 			System.out.println("Sending query: " + query);
 			final javax.persistence.Query q = this.em.createNativeQuery(query);
 			result = q.getFirstResult();
@@ -335,11 +298,11 @@ public class FileArchiveFacade extends AbstractFacade<FileArchive>
 
 	public List<FileArchive> findAllBuildingSiteFilesByUser(Users currentUser) {
 		List<ServiceContract> con = currentUser.getServiceContractList();
-		List<BuildingSite> bsList = new ArrayList<>(); 
-		for(ServiceContract sc : con){
+		List<BuildingSite> bsList = new ArrayList<>();
+		for (ServiceContract sc : con) {
 			bsList.add(sc.getBuildingSite());
 		}
-		
+
 		QFileArchive f = QFileArchive.fileArchive;
 		JPAQuery q = new JPAQuery(this.em);
 		q.from(f);
